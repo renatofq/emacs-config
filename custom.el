a54bcd36a104f2feee50a967cdd61b0078b94a7e
F8543002 Renato Fernandes de Queiroz <renatofq@bb.com.br>
2025-02-23 08:05:41 -0300

    update custom.el

--- custom.el
+++ custom.el
@@ -5,7 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
  '(connection-local-criteria-alist
-   '(((:application tramp :protocol "flatpak")
+   '(((:application vc-git) vc-git-connection-default-profile)
+     ((:application tramp :protocol "flatpak")
       tramp-flatpak-connection-local-default-profile)
      ((:application tramp :protocol "kubernetes")
       tramp-kubernetes-connection-local-default-profile)
@@ -13,7 +14,8 @@
      ((:application tramp) tramp-connection-local-default-system-profile
       tramp-connection-local-default-shell-profile)))
  '(connection-local-profile-alist
-   '((tramp-flatpak-connection-local-default-profile
+   '((vc-git-connection-default-profile (vc-git--program-version))
+     (tramp-flatpak-connection-local-default-profile
       (tramp-remote-path "/app/bin" tramp-default-remote-path "/bin" "/usr/bin"
                          "/sbin" "/usr/sbin" "/usr/local/bin" "/usr/local/sbin"
                          "/local/bin" "/local/freeware/bin" "/local/gnu/bin"
@@ -79,22 +81,16 @@
      (tramp-connection-local-default-system-profile (path-separator . ":")
                                                     (null-device . "/dev/null"))))
  '(custom-safe-themes
-   '("21e3d55141186651571241c2ba3c665979d1e886f53b2e52411e9e96659132d4"
+   '("a75aff58f0d5bbf230e5d1a02169ac2fbf45c930f816f3a21563304d5140d245"
+     "fbf73690320aa26f8daffdd1210ef234ed1b0c59f3d001f342b9c0bbf49f531c"
+     "2e7dc2838b7941ab9cabaa3b6793286e5134f583c04bde2fba2f4e20f2617cf7"
+     "21e3d55141186651571241c2ba3c665979d1e886f53b2e52411e9e96659132d4"
      "69f7e8101867cfac410e88140f8c51b4433b93680901bb0b52014144366a08c8"
      "f74e8d46790f3e07fbb4a2c5dafe2ade0d8f5abc9c203cd1c29c7d5110a85230"
      "2dc03dfb67fbcb7d9c487522c29b7582da20766c9998aaad5e5b63b5c27eec3f"
      "24168c7e083ca0bbc87c68d3139ef39f072488703dcdd82343b8cab71c0f62a7"
      "c8b83e7692e77f3e2e46c08177b673da6e41b307805cd1982da9e2ea2e90e6d7" default))
- '(package-selected-packages
-   '(cider clj-refactor clojure-mode cobol-mode consult corfu crux
-           diff-hl diminish eat edit-indirect editorconfig eglot
-           fish-mode flymake flymake-guile flymake-kondor geiser
-           geiser-guile gpel graphviz-dot-mode htmlize jinx js-comint
-           just-mode lsp-java lua-mode magit marginalia markdown-mode
-           meson-mode modus-themes olivetti orderless pcmpl-args paredit
-           plantuml-mode rainbow-delimiters rg smartparens tramp
-           typescript-mode undo-tree use-package vertico which-key
-           yasnippet))
+ '(package-selected-packages nil)
  '(safe-local-variable-values
    '((likec4-indent-offset . 2)
      (eval advice-add 'org-babel-insert-result :filter-args
