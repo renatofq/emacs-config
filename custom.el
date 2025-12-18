213e5032fca6b5f5e5b6503c615881a074027bea
F8543002 Renato Fernandes de Queiroz <renatofq@bb.com.br>
2025-12-18 08:57:16 -0300

    tira casaco/bota casaco no custom.el

--- custom.el
+++ custom.el
@@ -92,13 +92,7 @@
      "2dc03dfb67fbcb7d9c487522c29b7582da20766c9998aaad5e5b63b5c27eec3f"
      "24168c7e083ca0bbc87c68d3139ef39f072488703dcdd82343b8cab71c0f62a7"
      "c8b83e7692e77f3e2e46c08177b673da6e41b307805cd1982da9e2ea2e90e6d7" default))
- '(package-selected-packages
-   '(cider clojure-mode consult copilot corfu denote diminish eat edit-indirect
-           editorconfig ef-themes eglot-java eglot-tempel feature-mode fish-mode
-           flymake-kondor geiser geiser-guile gptel jinx js-comint just-mode
-           magit marginalia markdown-mode modus-themes move-text olivetti
-           orderless paredit pcmpl-args plantuml-mode rainbow-delimiters rg
-           tempel typescript-mode vertico vundo which-key))
+ '(package-selected-packages nil)
  '(package-vc-selected-packages
    '((difftastic :url "https://github.com/pkryger/difftastic.el.git")))
  '(safe-local-variable-values
